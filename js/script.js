ccf50a591ff6834357fcd21ca3fd04eb302d7525
Fares-Abdelkader <124618781+Fares-Abdelkader@users.noreply.github.com>
2023-05-30 02:39:28 +0000

    Index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Fares.Abdelkader
+// Created on: March 2022
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates volume and perimeter of rectangle.
+ */
+function calculate() {
+  // input
+  const length = parseInt(document.getElementById('length').value)
+  const width = parseInt(document.getElementById('width').value)
+  const height = parseInt(document.getElementById('height').value)
+
+  // process 
+  const volume = (length * width * height) / 3
+
+  // output
+  document.getElementById('volume').innerHTML = '<p>volume is: ' + volume.toString() + ' cm³</p>'
+}
